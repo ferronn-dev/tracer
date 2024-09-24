c1ae92286e0917c5497836769a2360db67914d8e
Ferronn <ferronn@ferronn.dev>
2024-09-23 20:18:58 -0500

    add tick and debug stack

--- tracer.lua
+++ tracer.lua
@@ -1,12 +1,15 @@
+local tick = 0
 local tracer = CreateFrame('Frame')
 tracer:RegisterAllEvents()
-tracer:SetScript('OnEvent', function(self, ev, ...)
-  local t = { { ev, ... } }
-  local n = 1
-  self:SetScript('OnEvent', function(_, ...)
-    n = n + 1
-    t[n] = { ... }
-  end)
+tracer:SetScript('OnUpdate', function()
+  tick = tick + 1
+end)
+tracer:SetScript('OnEvent', function(_, ...)
+  local t, n = { { tick, debugstack(2, 1000, 1000), ... } }, 1
   TracerSavedVariable = TracerSavedVariable or { sessions = {} }
   TracerSavedVariable.sessions[GetServerTime()] = { events = t }
+  tracer:SetScript('OnEvent', function(_, ...)
+    n = n + 1
+    t[n] = { tick, debugstack(2, 1000, 1000), ... }
+  end)
 end)
